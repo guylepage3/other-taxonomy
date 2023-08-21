21007c4fc0faffa5bbe3e91f4dc2a2c74e9ebc17
Guy Lepage <guylepage3@gmail.com>
2023-08-21 12:48:42 -0700

    billing card
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- components/billing-form.tsx
+++ components/billing-form.tsx
@@ -85,6 +85,36 @@ export function BillingForm({
           ) : null}
         </CardFooter>
       </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Subscription Plan</CardTitle>
+          <CardDescription>
+            You are currently on the <strong>{subscriptionPlan.name}</strong>{" "}
+            plan.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>{subscriptionPlan.description}</CardContent>
+        <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+          <button
+            type="submit"
+            className={cn(buttonVariants())}
+            disabled={isLoading}
+          >
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            {subscriptionPlan.isPro ? "Manage Subscription" : "Upgrade to PRO"}
+          </button>
+          {subscriptionPlan.isPro ? (
+            <p className="rounded-full text-xs font-medium">
+              {subscriptionPlan.isCanceled
+                ? "Your plan will be canceled on "
+                : "Your plan renews on "}
+              {formatDate(subscriptionPlan.stripeCurrentPeriodEnd)}.
+            </p>
+          ) : null}
+        </CardFooter>
+      </Card>
     </form>
   )
 }
